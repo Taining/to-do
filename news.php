fe773831bca16d3d6df9c428d0c33945209df0f0
tang <tangning111@gmail.com>
2014-02-18 16:17:32 +0800

    added news.php

---
+++ news.php
@@ -0,0 +1,33 @@
+<?php
+	session_save_path('sess');
+	session_start();
+
+	$page = "news";
+
+	if (isset($_SESSION['user'])) {
+		$userid = $_SESSION['user'];
+	} else {
+		header("Location: login.php");
+		exit;
+	}
+
+	require "config.inc";
+	require "header.php";
+		
+	$dbconn = pg_connect("host=127.0.0.1 port=5432 dbname=$db_name user=$db_user password=$db_password");
+	if (!$dbconn){
+		echo("Can't connect to the database");	
+		exit;
+	}
+
+	$result = pg_query($dbconn, "SELECT U.fname, T.dscrp, T.createtime FROM appuser U, tasks T WHERE U.uid=T.uid ORDER BY T.taskid DESC LIMIT 10;");
+	echo "<div class='container'>
+			<h2>News</h2>
+			<ol>";
+	while ($row = pg_fetch_array($result)) {
+		echo "<li>User '$row[fname]'' added task '$row[dscrp]'' on <code>$row[createtime]</code>.</li>";
+	}
+	echo "</ol></div>";
+
+	require 'footer.php';
+?>

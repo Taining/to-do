e14c35af93547de0f63b97fdd101d9ddce5aa7dc
TaiNing <tainingw@gmail.com>
2014-02-19 15:04:50 +0800

    css of contact.php

--- contact.php
+++ contact.php
@@ -15,10 +15,15 @@
 	require "header.php";
 ?>
 
-<div class="container">
-	<a href="mailto:taining.wang@nus.edu.sg">Wang Taining: taining.wang@nus.edu.sg</a><br>
-	<a href="mailto:tangning@nus.edu.sg">Tang Ning: tangning@nus.edu.sg</a>
-	<iframe src="//www.facebook.com/plugins/follow.php?href=http%3A%2F%2Fwww.facebook.com%2Fzuck&amp;width=50&amp;height=80&amp;colorscheme=light&amp;layout=standard&amp;show_faces=true" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:50px; height:80px;" allowTransparency="true"></iframe>
+<div class="container" id="contact">
+	<div class="person">
+	<span class='link'>Wang Taining: <a href="mailto:taining.wang@nus.edu.sg">taining.wang@nus.edu.sg</a></span><br>
+	<iframe src="//www.facebook.com/plugins/follow?href=https%3A%2F%2Fwww.facebook.com%2Ftaining.wang.3&amp;layout=standard&amp;show_faces=true&amp;colorscheme=light&amp;width=450&amp;height=80" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:80px;" allowTransparency="true"></iframe><br>
+	</div>
+	<div class="person">
+	<span class='link'>Tang Ning: <a href="mailto:tangning@nus.edu.sg">tangning@nus.edu.sg</a></span><br>
+	<iframe src="//www.facebook.com/plugins/follow.php?href=http%3A%2F%2Fwww.facebook.com%2Fzuck&amp;layout=standard&amp;show_faces=true" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:80px;" allowTransparency="true"></iframe>
+	</div>
 </div>
 
 <?php

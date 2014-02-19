10549ff14817ff33859197694b1994090ab1f5eb
tang <tangning111@gmail.com>
2014-02-19 15:30:34 +0800

    refactor login.php

--- login.php
+++ login.php
@@ -2,36 +2,37 @@
     session_save_path("sess");
 	session_start();
     
-    require 'config.inc';
     $page = "login";
+
+    require 'config.inc';
+    require 'header.php';
+
     $errMessage = "";
     $email = "";
     if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        if(isset($_POST['email']) && isset($_POST['password'])){
-            $dbconn = pg_connect("host=localhost port=5432 dbname=$db_name user=$db_user password=$db_password");
-            if(!$dbconn){
-                $errMessage = "Connect to server failed";
-                exit;
-            } 
-            
-            $email = $_POST['email'];
-            $password = md5($_POST['password']);
+        $dbconn = pg_connect("host=localhost port=5432 dbname=$db_name user=$db_user password=$db_password");
+        if(!$dbconn){
+            echo "Aw, Snap!";
+            exit;
+        } 
 
-            $select_user_query = ("SELECT * FROM appuser WHERE email = $1 AND password = $2;");
-            $result = pg_prepare($dbconn, "select_user", $select_user_query);
-            $result = pg_execute($dbconn, "select_user", array($email, $password));
-            
-            if(pg_num_rows($result)){
-                $row = pg_fetch_array($result);
-                $_SESSION['user'] = $row['uid'];
-                header("Location: home.php");
-            } else {
-                $errMessage = "Invalid user or password.";
-            }
+        $email = $_POST['email'];
+        $password = md5($_POST['password']);
+
+        $select_user_query = ("SELECT * FROM appuser WHERE email = $1 AND password = $2;");
+        $result = pg_prepare($dbconn, "select_user", $select_user_query);
+        $result = pg_execute($dbconn, "select_user", array($email, $password));
+
+        if(pg_num_rows($result)){
+            $row = pg_fetch_array($result);
+            $_SESSION['user'] = $row['uid'];
+            $_SESSION['fname'] = $row['fname'];
+            header("Location: home.php");
+        } else {
+            $errMessage = "Invalid user or password.";
         }
     }
     
-    require 'header.php';
 ?>  
 <div class="container">
     <form method = "POST">
